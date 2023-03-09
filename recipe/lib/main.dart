101bcd41e522193a424ddf3ec1864ec077cdec21
David Lee <mikeportnoy168@gmail.com>
2023-03-09 18:05:39 +0800

    feat: add recipe card and update it

--- recipe/lib/main.dart
+++ recipe/lib/main.dart
@@ -67,13 +67,37 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
           itemCount: Recipe.samples.length,
           itemBuilder: (BuildContext context, int index) {
-            // TODO: Update to return Recipe card
-            return Text(Recipe.samples[index].label);
+            // TODO: Add GestureDetector
+            return buildRecipeCard(Recipe.samples[index]);
           },
         ),
       ),
     );
   }
 
-  // TODO: Add buildRecipeCard() here
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
